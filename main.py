d8fd721e2bd8ac448e1d2fe291bddae10bfe563d
Lyudmila2910 <L_arkashina@mail.ru>
2025-05-13 16:02:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 G = nx.path_graph(20)
 G.add_edges_from([(0, 10)])
@@ -6,3 +7,4 @@ centrality = nx.eigenvector_centrality_numpy(G)
 for n in centrality:
   print ("c(", n, ")=", f"{centrality[n]:.8f}")
 nx.draw(G, with_labels = True)
+plt.show()
